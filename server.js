ecfc98d8fe43cb5ddaf49c6c19d8997c5aa9c25a
PallobST <pallobjahid.storexbd@gmail.com>
2024-06-08 05:09:51 +0600

    added cors package

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 // Serve the HTML file for the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, ()=>{
