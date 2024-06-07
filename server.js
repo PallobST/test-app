92373986629f24263c101234064fe6c02c12412a
PallobST <pallobjahid.storexbd@gmail.com>
2024-06-08 05:05:17 +0600

    added express functionalities

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path')
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+// Serve the HTML file for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Users App is listening on ${PORT}`)
+})
